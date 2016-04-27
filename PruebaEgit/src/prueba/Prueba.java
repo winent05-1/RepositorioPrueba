1ad407229d518441784dd95dea629188823cda07
Urtzi <urtzidc@gmail.com>
2016-04-27 16:26:56 +0200

    2ºCommit

--- PruebaEgit/src/prueba/Prueba.java
+++ PruebaEgit/src/prueba/Prueba.java
@@ -3,6 +3,7 @@ package prueba;
 public class Prueba {
 public Prueba(){
 	System.out.println("Hola 1");
+	System.out.println("Hola 2");
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

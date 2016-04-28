37966466d685d12130aef870c9e12eb6d856ddba
winent05-1 <urtzidc@gmail.com>
2016-04-28 13:02:55 +0200

    Update Prueba.java
    3ºCommit

--- PruebaEgit/src/prueba/Prueba.java
+++ PruebaEgit/src/prueba/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 public Prueba(){
 	System.out.println("Hola 1");
 	System.out.println("Hola 2");
-	System.out.println("Hola 3");
+	System.out.println("Hola 3 ");
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
